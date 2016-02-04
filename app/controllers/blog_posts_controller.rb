abbb4c20645c30b5a2e674c7a4b23eb1976a418b
Robert Cunningham <rgc3dev@gmail.com>
2016-02-04 10:27:35 -0500

    add details to users, work on forms

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -74,6 +74,6 @@ class BlogPostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_post_params
-      params.require(:blog_post).permit(:title, :author, :blog_entry, :blog_post_id)
+      params.require(:blog_post).permit(:title, :author, :blog_entry, :blog_post_id, :user_id)
     end
 end
