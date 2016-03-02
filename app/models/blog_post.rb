987f30e00592eda9ce07bbdced44ba730c4c3a8a
Robert Cunningham <rgc3dev@gmail.com>
2016-03-02 08:56:24 -0500

    Set slugs for duplicate post titles

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,14 @@
 class BlogPost < ActiveRecord::Base
 	extend FriendlyId
-		friendly_id :title, use: :slugged
+		friendly_id :slug_candidates, use: :slugged
+
+			def slug_candidates
+				[
+					:title,
+					[:title, :user_id],
+				]
+			end
+
 	belongs_to :user
 	has_many :comments
 end
